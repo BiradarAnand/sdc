ea732a8767b5728f7f7fcac0284a0d94668be61f
Biradar Anand <biradaranand025@gmail.com>
2025-08-13 15:06:44 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+// 
+/* write a javascript progam to determine largest of three numbers using nested if else statement
+using switch statement display day of this week based on number */
+let a=prompt("Enter first value:")
+let b=prompt("Enter second value:")
+let c=prompt("Enter third value:")
+x=Number(a)
+y=Number(b)
+z=Number(c)
+if (x>y && x>z){
+    console.log("Largest number is",x)
+}
+else if
+    (y>x && y>z){
+    console.log("Largest number is",y)
+}
+else{
+    console.log("Largest number is",z)
+}
